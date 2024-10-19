3b5ff8e03ccd6de56e494d7173ce44cf969bb09c
Pranay Khobragade <pranayk7@gmail.com>
2024-10-19 22:00:01 +0530

    Covered default method in java8 interface

--- src/com/pk/service/InterfaceA.java
+++ src/com/pk/service/InterfaceA.java
@@ -1,5 +1,12 @@
 package com.pk.service;
 
+// In java8 interface we can define the methods
+// We can use this terminology to add new methods to existing interface
 public interface InterfaceA {
+	
+//	The methods can be defined using default keyword
+	default void show() {
+		System.out.println("Interface A show method");
+	}
 
 }
--- src/com/pk/service/impl/InterfaceAImpl.java
+++ src/com/pk/service/impl/InterfaceAImpl.java
@@ -2,6 +2,7 @@ package com.pk.service.impl;
 
 import com.pk.service.InterfaceA;
 
+// Default methods are not compulsory to implement on implemented classes
 public class InterfaceAImpl implements InterfaceA {
 
 }
